628ab1248aaa74e7adb1545c123117e57b3f706a
Dayo Opedare <djetlii@live.com>
2021-08-12 15:31:46 +0100

    deploy

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
